59bd95172a256d2378c77a7e05a3d02568a97bad
Marius Ilie <marius@iliemarius.ro>
2021-01-19 15:55:46 +0200

    Switch to microservices

---
+++ Microservices/run_all.sh
@@ -0,0 +1,5 @@
+cd BogusApp-Microservices-ApiGateway && swift run > /dev/null 2>&1 & 
+cd BogusApp-Microservices-Benefits && swift run > /dev/null 2>&1 & 
+cd BogusApp-Microservices-Fees && swift run > /dev/null 2>&1 & 
+cd BogusApp-Microservices-Channels && swift run > /dev/null 2>&1 & 
+cd BogusApp-Microservices-TargetSpecifics && swift run > /dev/null 2>&1 & 
