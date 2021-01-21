e484fffac3c3d24b1fc1ac79ab30b0929738a945
Marius Ilie <marius@iliemarius.ro>
2021-01-21 23:37:44 +0200

    Remove fees + add channels microservice

--- Microservices/run_all.sh
+++ Microservices/run_all.sh
@@ -1,5 +1,4 @@
 cd BogusApp-Microservices-ApiGateway && swift run > /dev/null 2>&1 & 
-cd BogusApp-Microservices-Benefits && swift run > /dev/null 2>&1 & 
-cd BogusApp-Microservices-Fees && swift run > /dev/null 2>&1 & 
-cd BogusApp-Microservices-Channels && swift run > /dev/null 2>&1 & 
-cd BogusApp-Microservices-TargetSpecifics && swift run > /dev/null 2>&1 & 
+cd ../BogusApp-Microservices-Benefits && swift run > /dev/null 2>&1 &
+cd ../BogusApp-Microservices-Channels && swift run > /dev/null 2>&1 &
+cd ../BogusApp-Microservices-TargetSpecifics && swift run > /dev/null 2>&1 &
